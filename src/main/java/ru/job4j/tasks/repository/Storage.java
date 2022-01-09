99f2ffcfc5e55885255e70b1341b711afe88f7bb
k-r-3 <rke19922802@gmail.com>
2022-01-09 21:05:27 +0500

    Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/tasks/repository/Storage.java
+++ src/main/java/ru/job4j/tasks/repository/Storage.java
@@ -2,6 +2,7 @@ package ru.job4j.tasks.repository;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -11,6 +12,7 @@ import ru.job4j.tasks.models.Task;
 
 import java.io.Closeable;
 import java.util.List;
+import java.util.function.Function;
 
 public class Storage implements Closeable, Store {
     private final static Logger LOG = LoggerFactory.getLogger(Storage.class);
@@ -43,59 +45,67 @@ public class Storage implements Closeable, Store {
         this.sc = new MetadataSources(reg).buildMetadata().buildSessionFactory();
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sc.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     @Override
     public void add(Task task) {
-        Session session = sc.openSession();
-        session.beginTransaction();
-        int id = (int) session.save(task);
-        session.createQuery("insert into Task (descr) select :param from Task")
-        .setParameter("param", task.getDescr());
-        session.getTransaction().commit();
+        this.tx(
+                session -> {
+                    session.save(task);
+                    return session.createQuery("insert into Task (descr) select :param from Task")
+                            .setParameter("param", task.getDescr());
+                }
+        );
     }
 
     @Override
     public Task find(String id) {
-        Session session = sc.openSession();
-        session.beginTransaction();
-        Task rsl = session.find(Task.class, id);
-        session.getTransaction().commit();
-        return rsl;
+        return this.tx(
+                session -> session.find(Task.class, id)
+        );
     }
 
     @Override
     public List<Task> findUndone() {
-        Session session = sc.openSession();
-        session.beginTransaction();
-        List rsl = session.createQuery("from ru.job4j.tasks.models.Task where done = :paramDone")
+        return this.tx(
+                session -> session.createQuery("from ru.job4j.tasks.models.Task "
+                        + "where done = :paramDone")
                 .setParameter("paramDone", false)
-                .list();
-        session.getTransaction().commit();
-        return rsl;
+                .list()
+        );
     }
 
     @Override
     public List<Task> findDone() {
-        Session session = sc.openSession();
-        session.beginTransaction();
-        List<Task> rsl = session.createQuery("from ru.job4j.tasks.models.Task "
+        return this.tx(
+                session -> session.createQuery("from ru.job4j.tasks.models.Task "
                 + "where done = :paramDone")
                 .setParameter("paramDone", true)
-                .list();
-        session.getTransaction().commit();
-        return rsl;
+                .list()
+        );
     }
 
     @Override
     public boolean replace(String id, boolean status) {
-        boolean rsl = false;
-        Session session = sc.openSession();
-        session.beginTransaction();
-        rsl = session.createQuery("update ru.job4j.tasks.models.Task set done = :paramDone "
-                + "where id = :paramId")
+        return this.tx(
+                session -> session.createQuery("update ru.job4j.tasks.models.Task"
+                        + " set done = :paramDone where id = :paramId")
                 .setParameter("paramId", Integer.parseInt(id))
                 .setParameter("paramDone", status)
-                .executeUpdate() != 0;
-        session.getTransaction().commit();
-        return rsl;
+                .executeUpdate() != 0
+        );
     }
 }
